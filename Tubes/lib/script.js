1c3dd96fe738997e57a9caf4691bc46ad90e75bf
farrelkumara <67863604+farrelkumara@users.noreply.github.com>
2021-06-07 19:03:30 +0700

    add control keyboard >.<

--- Tubes/lib/script.js
+++ Tubes/lib/script.js
@@ -31,8 +31,8 @@ controls.update();
 
 let loader = new THREE.GLTFLoader();
 loader.load("models/scene.gltf",function(gltf){
-    scene.add(gltf.scene);
-    renderer.render(scene,cam);
+    virus = gltf.scene;
+    scene.add(virus);
 });
 
 let grid = new THREE.GridHelper(100, 20, 0xfafafa, 0xfafafa);
@@ -47,11 +47,36 @@ window.addEventListener('resize', function() {
     renderer.setSize(width, height);
     cam.aspect = width / height;
     cam.updateProjectionMatrix();
-})
+});
+
+let keyboard = [];
+
+document.body.onkeydown = function(evt) {
+    keyboard[evt.key] = true;
+}
+document.body.onkeyup = function(evt) {
+    keyboard[evt.key] = false;
+}
+
+function process_keyboard() {
+    if (keyboard['a']){
+        virus.position.x -= 0.07;
+    }
+    else if (keyboard['d']){
+        virus.position.x += 0.07;
+    }
+    if (keyboard['s']){
+        virus.position.z += 0.07;
+    }
+    else if (keyboard['w']){
+        virus.position.z -= 0.07;
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene,cam);
+    process_keyboard();
 }
 animate()
 
