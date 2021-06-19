85a38ed5fb66911b3ad874702155a5743ea2ca7b
aldojr17 <aldojr17@users.noreply.github.com>
2021-06-19 14:53:30 +0900

    Random Position Virus

--- Tubes/lib/script.js
+++ Tubes/lib/script.js
@@ -1,17 +1,14 @@
 var scene = new THREE.Scene();
 const aspect = window.innerWidth / window.innerHeight;
 const windowSize = 20;
+var randomPos = [10, 0, -10];
 
-var cam = new THREE.PerspectiveCamera(60, window.innerWidth/window.innerHeight, 1, 1000);
-
-// let cam = new THREE.OrthographicCamera(
-//   -windowSize * aspect,
-//   windowSize * aspect,
-//   windowSize,
-//   -windowSize,
-//   -100,
-//   100
-// );
+var cam = new THREE.PerspectiveCamera(
+  60,
+  window.innerWidth / window.innerHeight,
+  1,
+  1000
+);
 
 var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -29,11 +26,14 @@ let loader2 = new THREE.GLTFLoader();
 loader2.load("model_virus/scene.gltf", function (gltf) {
   virus = gltf.scene;
   virus.scale.set(0.5, 0.5, 0.5);
-  virus.position.set(0, 0, -10);
+  virus.position.set(
+    randomPos[Math.floor(Math.random() * randomPos.length)],
+    0,
+    -10
+  );
   scene.add(virus);
 });
 
-var mixer;
 let loader = new THREE.GLTFLoader();
 loader.load("model_person/scene.gltf", function (gltf) {
   person = gltf.scene;
@@ -46,6 +46,10 @@ loader.load("model_person/scene.gltf", function (gltf) {
   action.play();
 });
 
+var virus = new THREE.Object3D();
+var person = new THREE.Object3D();
+var mixer = new THREE.AnimationMixer(person);
+
 //light untuk person
 var light = new THREE.HemisphereLight(0xffffff, 0x000000, 2);
 scene.add(light);
@@ -54,12 +58,8 @@ let grid = new THREE.GridHelper(100, 10, 0xfafafa, 0xfafafa);
 grid.position.y = -1;
 scene.add(grid);
 
-// cam.position.set(5, 3, 4);
 cam.position.z = 35;
 cam.position.y = 10;
-// cam.position.set(5, 3, 4);
-// cam.rotation.x = 6;
-// cam.rotation.y = 1;
 
 var controls = new THREE.OrbitControls(cam, renderer.domElement);
 controls.update();
@@ -83,40 +83,21 @@ window.addEventListener("resize", function () {
   cam.updateProjectionMatrix();
 });
 
-// let keyboard = [];
-
-// document.body.onkeydown = function (evt) {
-//   keyboard[evt.key] = true;
-// };
-// document.body.onkeyup = function (evt) {
-//   keyboard[evt.key] = false;
-// };
-
 document.body.onkeypress = function (evt) {
   if (evt.key == "a") {
-    person.position.x -= 10;
+    person.position.x != -10
+      ? (person.position.x -= 10)
+      : (person.position.x = -10);
   } else if (evt.key == "d") {
-    person.position.x += 10;
+    person.position.x != 10
+      ? (person.position.x += 10)
+      : (person.position.x = 10);
   } else if (evt.key == " ") {
-    person.position.y += 10;
+    person.position.y == 0
+      ? (person.position.y += 10)
+      : (person.position.y = 10);
   }
 };
-
-// function process_keyboard() {
-//   if (keyboard["a"]) {
-//     person.position.x -= 0.5;
-//   } else if (keyboard["d"]) {
-//     person.position.x += 0.5;
-//   } else if (keyboard[" "]) {
-//     person.position.y = 10;
-//   }
-//   // if (keyboard['s']){
-//   //     virus.position.z += 0.10;
-//   // }
-//   // else if (keyboard['w']){
-//   //     virus.position.z -= 0.10;
-//   // }
-// }
 
 // let starGeo, stars;
 // function backgroundAnimate() {
@@ -143,11 +124,27 @@ document.body.onkeypress = function (evt) {
 //   animate();
 // }
 
+// var randomPos = [-10, 0, 10];
+// randomPos[Math.floor(Math.random()*randomPos.length)];
 
-function obstacles() {}
+var unused = [];
 
+function obstacles() {
+  // loader2.load("model_virus/scene.gltf", function (gltf) {
+  //   virus2 = gltf.scene;
+  //   virus2.scale.set(0.5, 0.5, 0.5);
+  //   virus2.position.set(
+  //     randomPos[Math.floor(Math.random() * randomPos.length)],
+  //     0,
+  //     -10
+  //   );
+  //   scene.add(virus2);
+  // });
+  console.log("test");
+}
+// obstacles();
 const clock = new THREE.Clock();
-
+let i = 0;
 function animate() {
   // starGeo.vertices.forEach(p=>{
   //   p.velocity += p.acceleration;
@@ -159,25 +156,38 @@ function animate() {
   // });
   // starGeo.verticesNeedUpdate = true;
   // stars.rotation.y += 0.002;
-  requestAnimationFrame(animate);
-
-  // virus.position.z += 0.1;
+  if (
+    Math.round(virus.position.z) == 19 &&
+    person.position.x == virus.position.x
+  ) {
+    alert("stop");
+  } else {
+    requestAnimationFrame(animate);
+  }
 
   Math.round(person.position.y) != 0
     ? (person.position.y -= 0.25)
     : (person.position.y = 0);
 
   Math.round(virus.position.z) == 30
-    ? (virus.position.z = -10)
+    ? virus.position.set(
+        randomPos[Math.floor(Math.random() * randomPos.length)],
+        0,
+        -10
+      )
     : (virus.position.z += 0.1);
-  //   person.position.z -= 0.1;
-  //   cam.position.z -= 0.1;
 
-  const delta = clock.getDelta();
+  var delta = clock.getDelta();
   mixer.update(delta);
 
   renderer.render(scene, cam);
-  //   process_keyboard();
+  document.getElementById("score").innerHTML = i++;
 }
+
+function draw() {
+  obstacles();
+}
+
 animate();
-backgroundAnimate();
+setInterval(draw, 5000);
+// backgroundAnimate();
